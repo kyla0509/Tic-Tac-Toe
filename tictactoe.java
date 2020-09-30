87b71afdae714d7ea31f0e3a141cba04cf4e3f6c
Kyla <57500265+kyla0509@users.noreply.github.com>
2020-09-30 01:12:40 -0400

    Being printing out Tic Tac Toe

---
+++ tictactoe.java
@@ -0,0 +1,28 @@
+import java.util.*;
+
+public class tictactoe {
+
+  public static void main(String []args) {
+        System.out.println("Welcome to Tic-Tac-Toe!");
+        System.out.println("");
+
+        // print grid
+        System.out.println("   |   |   ");
+        System.out.println("---|---|---");
+        System.out.println("   |   |   ");
+        System.out.println("---|---|---");
+        System.out.println("   |   |   ");
+
+        System.out.println("");
+        System.out.println("");
+        System.out.println("Here are the spaces you can fill in!");
+
+        // denotes what space is which
+        System.out.println("A1  B1  C1");
+        System.out.println("A2  B2  C2");
+        System.out.println("A3  B3  C3");
+
+        System.out.println("Enter your first move:");
+
+      }
+}
